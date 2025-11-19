77e7a121732edae75c957c758acc7c117f4abfc5
Your Name <you@example.com>
2025-11-19 16:00:11 +0200

    Wrote not working history code

--- app.js
+++ app.js
@@ -10,17 +10,21 @@ function KMIK(){
     personWeight = humanWeight.value
     personHeight = humanHeight.value * 2 * 0.01
     BMI = personWeight / personHeight
+    console.log(BMI)
     console.log(BMI.toFixed(2))
-
- if(BMI > 18,5){
-     mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Nepietiekama ķermeņa masa)"
-    }else if(BMI <= 18,5){
-     mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Normāla ķermeņa masa)"
-    }else if(BMI >= 25 ){
-     mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Lieka ķermeņa masa)"
-    }else if(BMI > 30 ){
+    if(BMI > 18,5){
+     mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Nepietiekama ķermeņa masa)"}
+    if(BMI <= 18,5){
+     mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Normāla ķermeņa masa)"}
+    if(BMI <= 25 ){
+    mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Lieka ķermeņa masa)"}
+    if(BMI > 30 ){
     mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Aptaukošanās)"}
-    addToHistory(BMI,personWeight,personHeight.toFixed(2));
-
+addToHistory(BMI,humanHeight,humanWeight.toFixed(2));
 }
 
+function addToHistory(BMI,humanHeight,humanWeight) {
+    const li = document.createElement("li");
+    li.textContent = addToHistory.BMI + "Augums" + addToHistory.humanHeight + "cm" + "Masa"+ addToHistory.humanWeight + "kg" ;
+    history.prepend(li);
+}
