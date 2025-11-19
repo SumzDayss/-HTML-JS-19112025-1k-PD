a29bdfbca8f99597dd6113fef68a9b78f5c29927
Your Name <you@example.com>
2025-11-19 15:40:49 +0200

    Added if, elseif to understand if youre overweight, underweight.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const humanWeight = document.getElementById("weight")
 const humanHeight = document.getElementById("tall")
 const mainButton = document.getElementById("button")
 const mainOutput = document.getElementById("output")
+const HistoryList = document.getElementById("history")
 
 mainButton.addEventListener("click",KMIK)
 
@@ -9,6 +10,17 @@ function KMIK(){
     personWeight = humanWeight.value
     personHeight = humanHeight.value * 2 * 0.01
     BMI = personWeight / personHeight
-    console.log(BMI)
+    console.log(BMI.toFixed(2))
+
+ if(BMI > 18,5){
+     mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Nepietiekama ķermeņa masa)"
+    }else if(BMI <= 18,5){
+     mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Normāla ķermeņa masa)"
+    }else if(BMI >= 25 ){
+     mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Lieka ķermeņa masa)"
+    }else if(BMI > 30 ){
+    mainOutput.innerText = "Jūsu ĶMI ir " +  BMI.toFixed(2) + " (Aptaukošanās)"}
+    addToHistory(BMI,personWeight,personHeight.toFixed(2));
 
 }
+
